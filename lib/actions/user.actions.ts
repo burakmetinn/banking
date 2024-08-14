244de9f901db9702cf006d120b7175e9f9681a59
burakmetinn <burakmetin579@gmail.com>
2024-08-14 23:06:31 +0300

    implement plaid and sentry

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { cookies } from "next/headers";
 import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { parseStringify } from "../utils";
+import { CountryCode, Products } from "plaid";
+import { plaidClient } from "@/lib/plaid";
 
 export const signIn = async ({email, password} : signInProps) => {
     try {
@@ -64,4 +66,28 @@ export const logoutAccount = async () => {
     } catch (error) {
         return null;
     }
+}
+
+export const createLinkToken = async (user: User) => {
+    try {
+        const tokenParams = {
+            user: {
+                client_user_id : user.$id
+            },
+            client_name: user.name,
+            products: ["auth"] as Products[],
+            language: "en",
+            country_codes: ["US"] as CountryCode[],
+        }
+
+        const response = await plaidClient.linkTokenCreate(tokenParams);
+        
+        return parseStringify({linkToken: response.data.link_token})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const exchangePublicToken = async () => {
+    
 }
